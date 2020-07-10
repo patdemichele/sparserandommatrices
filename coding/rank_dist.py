3432bf3b95fa7f38b68044e3e4934a35dacc4024
Patrick DeMichele <patd998@gmail.com>
2020-07-09 17:44:12 -0700

    severly bad lower bound

--- coding/rank_dist.py
+++ coding/rank_dist.py
@@ -1,10 +1,10 @@
-def compute_P(N, d, R, K): # computes P(r,k) for r<=R k<=K wrt N d
-  P = {}
-  P[(0,0)] = 1
+def compute_Q(N, d, R, K): # computes Q(r,k) \leq P(r,k) for r<=R k<=K wrt N d
+  Q = {}
+  Q[(0,0)] = 1
   for r in range(1,R+1):
-    P[(r,0)]=0
+    Q[(r,0)]=0
   for k in range(1,K+1):
-    P[(0,k)] = (1-d*1.0/N)**(N * k)
+    Q[(0,k)] = (1-d*1.0/N)**(N * k)
     for r in range(1,R+1):
-      P[(r,k)]= P[(r,k-1)] * (1-d*1.0/N)**(N-r) + P[(r-1,k-1)] * (1- (1-d*1.0/N)**(N-(r-1)))
-  return P
+      Q[(r,k)]= Q[(r,k-1)] * (1-d*1.0/N)**(N) + Q[(r-1,k-1)] * (1- (1-d*1.0/N)**(N-(r-1)))
+  return Q
