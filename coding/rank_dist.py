d28459b631c067694ffe979d0efa592405b0f016
Patrick DeMichele <patd998@gmail.com>
2020-07-09 17:28:55 -0700

    working (approx) P(r,k) code

--- coding/rank_dist.py
+++ coding/rank_dist.py
@@ -2,10 +2,9 @@ def compute_P(N, d, R, K): # computes P(r,k) for r<=R k<=K wrt N d
   P = {}
   P[(0,0)] = 1
   for r in range(1,R+1):
-    P[(0,r)]=0
+    P[(r,0)]=0
   for k in range(1,K+1):
-    P[(k,0)] = (1-d/N)^N
+    P[(0,k)] = (1-d*1.0/N)**(N * k)
     for r in range(1,R+1):
-    	P[(r,k)]= P[(r,k-1)] * (1-d*1.0/N)**(N-r) 
-    	+ P[(r-1,k-1)] * (1- (1-d*1.0/N)**(N-(r-1)))
+      P[(r,k)]= P[(r,k-1)] * (1-d*1.0/N)**(N-r) + P[(r-1,k-1)] * (1- (1-d*1.0/N)**(N-(r-1)))
   return P
